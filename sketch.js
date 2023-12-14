7547210f4f1b8d1b89bd7f154099775cc15e1665
Jan Ramdohr <jan.ramdohr@outlook.com>
2023-12-14 13:47:56 +0100

    add shader rendering

--- sketch.js
+++ sketch.js
@@ -1,46 +1,77 @@
 let tileSize = 1;
 let scl = 0.01;
 let wlevel = 0.3;
+let hMap, cMap, screenMap;
+let sun;
+let shadowShader;
+
+function preload() {
+  shadowShader = loadShader(
+    "/shaders/vertex_shader.glsl",
+    "/shaders/frac_shader.glsl"
+  );
+}
 
 function setup() {
   createCanvas(600, 600);
   noSmooth();
-  noStroke();
+
+  hMap = createGraphics(width / tileSize, height / tileSize);
+  cMap = createGraphics(width / tileSize, height / tileSize);
+  screenMap = createGraphics(width / tileSize, height / tileSize, WEBGL);
+
+  sun = createVector(0, 0.5, 1);
 
   genTerrain();
+
+  screenMap.shader(shadowShader);
 }
 
-function draw() {}
+function draw() {
+  shadowShader.setUniform("colour", cMap);
+  shadowShader.setUniform("height", hMap);
+  shadowShader.setUniform("sunPos", [sun.x, sun.y, sun.z]);
+
+  screenMap.clear();
+  screenMap.rect(0, 0, width / tileSize, height / tileSize);
+
+  image(screenMap, 0, 0, width, height);
+}
 
 function genTerrain() {
   for (var i = 0; i < width / tileSize; i++) {
     for (var j = 0; j < height / tileSize; j++) {
-      fill(calcColor(i, j));
-      rect(i * tileSize, j * tileSize, tileSize, tileSize);
+      calcTile(i, j);
     }
   }
 }
 
-function calcColor(x, y) {
+function calcTile(x, y) {
   const v = calcNoise(x, y);
 
+  hMap.noStroke();
+  hMap.fill(255 * v);
+  hMap.square(x, y, 1);
+
+  cMap.noStroke();
   if (v <= 0.3) {
-    return color("#62A6A9");
+    cMap.fill(color("#62A6A9"));
   } else if (v <= 0.4) {
-    return color("#D6B69E");
+    cMap.fill(color("#D6B69E"));
   } else if (v <= 0.5) {
-    return color("#98AD5A");
+    cMap.fill(color("#98AD5A"));
   } else if (v <= 0.6) {
-    return color("#658541");
+    cMap.fill(color("#658541"));
   } else if (v <= 0.7) {
-    return color("#477645");
+    cMap.fill(color("#477645"));
   } else if (v <= 0.8) {
-    return color("#6D7687");
+    cMap.fill(color("#6D7687"));
   } else if (v <= 0.9) {
-    return color("#848D9A");
+    cMap.fill(color("#848D9A"));
   } else {
-    return color("#D2E0DE");
+    cMap.fill(color("#D2E0DE"));
   }
+  cMap.square(x, y, 1);
 }
 
 function calcNoise(x, y) {
